5aa2fb65c617533899b09d4e5d97b024fcd0e5ca
arshad <arshad@holidify.com>
2025-11-29 16:19:40 +0530

    adding necessary schemas related to checkout and discoutn

--- app/models/schema.py
+++ app/models/schema.py
@@ -2,10 +2,19 @@
 Pydantic models for request/response validation and serialization.
 These models define the API contract and ensure type safety.
 """
+import uuid
 
 from pydantic import BaseModel, Field, field_validator
-from typing import List
+from typing import List, Optional
+from datetime import datetime
 
+
+class User(BaseModel):
+    session_id: str
+    date_created: Optional[datetime]
+
+
+# Cart Related Models
 class CartItem(BaseModel):
     """Represents an item in the shopping cart."""
 
@@ -43,3 +52,61 @@ class UpdateCartRequest(BaseModel):
 
     product_id: int = Field(..., gt=0)
     quantity: int = Field(..., gt=0)
+
+
+# Checkout Related Models
+class CheckoutRequest(BaseModel):
+    """Request model for checkout."""
+    discount_code: Optional[str] = Field(None, max_length=50)
+
+    @field_validator('discount_code')
+    @classmethod
+    def validate_discount_code(cls, v: Optional[str]) -> Optional[str]:
+        """Normalize discount code to uppercase."""
+        return v.upper() if v else None
+
+
+class CheckoutResponse(BaseModel):
+    """Response model for successful checkout."""
+
+    order_id: str
+    subtotal: float
+    discount_applied: bool
+    discount_amount: float = 0.0
+    total_amount: float
+    new_discount_code: Optional[str] = None
+    message: str
+    timestamp: datetime
+
+
+class Order(BaseModel):
+    order_id: str
+    items: List[CartItem]
+    subtotal: float
+    discount_code: Optional[str]
+    discount_amount: float
+    discount_percent: float
+    total_amount: float
+    timestamp: datetime
+
+
+class UserOrder(BaseModel):
+    order_count: int
+    orders: List[Order]
+
+
+class UsedDiscount(BaseModel):
+    order_id: str
+    discount_code: str
+    discount_percent: float
+    discount_amount: float
+    timestamp: datetime
+
+class Discount(BaseModel):
+    discount_code: str
+    discount_percent: float
+
+
+class UserDiscountData(BaseModel):
+    available_discount: Optional[Discount]
+    used_discounts: List[UsedDiscount]
